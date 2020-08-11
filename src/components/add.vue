e872edffdb47220852db015c74337e89597478ec
Suizhazha <573505403@qq.com>
2020-08-11 17:08:47 +0800

    生命钩子演示，看console

---
+++ src/components/add.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    {{ n }}
+    <button @click="add">+1</button>
+  </div>
+</template>
+
+<script >
+
+
+export default {
+
+  data: function () {
+    return {
+      n: 0
+    }
+  },
+  methods: {
+    add() {
+      this.n += 1
+    }
+  },
+  created() {
+    console.log("生成了")
+  },
+  updated() {
+    console.log("更新了")
+    console.log(this.n)
+  },
+  destroyed() {
+
+    console.log('消亡了')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/destroyed.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="toggle">toggle</button>
+    <hr>
+    <Add v-if="visible===true"/>
+  </div>
+</template>
+
+<script>
+
+import Add from "@/components/add"
+export default {
+  components: {Add},
+  data(){
+    return  {
+      visible:true
+    }
+  },
+methods:{
+    toggle(){
+
+this.visible = !this.visible
+      console.log(this.visible);
+    }
+}
+
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
